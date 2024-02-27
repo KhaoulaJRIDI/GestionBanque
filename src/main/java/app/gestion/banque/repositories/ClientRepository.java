357e47d1e2fc17e2468a65bf98447703d78f3ca0
Khaoula Jridi <khaoulajri@gmail.com>
2024-02-27 09:47:32 +0100

    commit1

---
+++ src/main/java/app/gestion/banque/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package app.gestion.banque.repositories;
+
+import app.gestion.banque.entities.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientRepository extends JpaRepository<Client,Long> {
+
+   // public Page<Client> findByNomClientContains(String nomClient, Pageable pageable);
+}
